48685c2fc68f91f075896359a6a546a89d8f8753
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-16 10:12:03 +0800

    add test cases and make it a controled input element

--- src/DebouncedInput.tsx
+++ src/DebouncedInput.tsx
@@ -6,14 +6,29 @@ export interface Props {
     debounce: number;
 }
 
-export class DebouncedInput extends React.Component<Props, {}> {
+export interface State {
+    val?: string;
+}
+
+export class DebouncedInput extends React.Component<Props, State> {
     private last_id: number = -1;
+
+    constructor(props?: Props, context?: any) {
+	super(props, context);
+
+	this.state = { val: this.props.value };
+    }
+
+    componentWillReceiveProps(props: Props) {
+	this.setState({ val: this.props.value });
+    }
 
     onChange = (e: Event) => {
 	let id = ++this.last_id;
 	let val = (e.target as HTMLInputElement).value;
 
 	setTimeout(() => { this.validate(id, val) }, this.props.debounce);
+	this.setState({ val: val });
     };
 
     validate(id: number, val: string) {
@@ -25,6 +40,6 @@ export class DebouncedInput extends React.Component<Props, {}> {
     }
 
     render() {
-	return <input onChange={this.onChange} value={this.props.value} />;
+	return <input onChange={this.onChange} value={this.state.val} />;
     }
 }
--- test/DebouncedInput.spec.tsx
+++ test/DebouncedInput.spec.tsx
@@ -14,4 +14,38 @@ describe("<DebouncedInput />", () => {
 	let wrapper = shallow(<DebouncedInput onChange={() => {}} debounce={100} />);
 	expect(wrapper.is("input")).to.be.true;
     });
+    it("changes value of input when props.value changes", () => {
+	let wrapper = shallow(<DebouncedInput onChange={() => {}} debounce={100} value="test" />);
+	expect(wrapper.find("input").prop("value")).to.equal("test");
+    });
+    it("changes value of input immediately after change event, regarding debounce settings", () => {
+	let wrapper = shallow(<DebouncedInput onChange={() => {}} debounce={100} value="test" />);
+	wrapper.simulate("change", {target: {value: "test2"}});
+	expect(wrapper.find("input").prop("value")).to.equal("test2");
+    });
+    it("delays 100ms if debounce=100, discarding old one, onChange should not be called before that", async () => {
+	let spy = sinon.spy();
+	let wrapper = shallow(<DebouncedInput onChange={spy} debounce={100} />);
+	wrapper.simulate("change", {target: {value: "test"}});
+	await new Promise((s,j) => setTimeout(s, 50));
+	expect(spy).not.to.be.called;
+	
+	wrapper.simulate("change", {target: {value: "test"}});
+	await new Promise((s,j) => setTimeout(s, 50));
+	expect(spy).not.to.be.called;
+	
+	await new Promise((s,j) => setTimeout(s, 50));
+	expect(spy).to.be.calledOnce;
+    });
+    it("delivers newest result only", async () => {
+	let spy = sinon.spy();
+	let wrapper = shallow(<DebouncedInput onChange={spy} debounce={100} />);
+	wrapper.simulate("change", {target: {value: "test"}});
+	await new Promise((s,j) => setTimeout(s, 50));
+	
+	wrapper.simulate("change", {target: {value: "test2"}});
+	await new Promise((s,j) => setTimeout(s, 100));
+	
+	expect(spy).to.be.calledWith("test2");
+    });
 });
