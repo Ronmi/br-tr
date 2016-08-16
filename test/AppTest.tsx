bf492ff65c62aca2fbc1e61d6c2b12275139e56c
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-16 12:17:38 +0800

    update app test

--- test/AppTest.tsx
+++ test/AppTest.tsx
@@ -10,8 +10,8 @@ import App from "../src/App";
 const expect = chai.expect;
 
 describe("<App />", () => {
-  it("is a h1", () => {
+  it("is a Branch", () => {
     let wrapper = shallow(<App />);
-    expect(wrapper.is("h1")).to.be.true;
+    expect(wrapper.is("Branch")).to.be.true;
   });
 });
