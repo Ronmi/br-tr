33381775578faf12a72a89038b29d8312292db4e
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-16 16:20:30 +0800

    prepare manual test for <Project />

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import * as React from "react";
-import Branches from "./Branches";
+import Project from "./Project";
 
 export default class App extends React.Component<{}, {}> {
     render() {
-	let props = {
-	    descChanged: (repo: string, desc: string) => {
-		return new Promise<void>((s,j) => {
-		    setTimeout(s, 3000);
-		});
-	    },
-	    ownerChanged: (repo: string, owner: string) => {
-		return new Promise<void>((s,j) => {
-		    setTimeout(s, 3000);
-		});
-	    },
-	    name: "test/repo",
-	    branches: [
-		{name: "b1", owner: "o1", desc: "d1"},
-		{name: "b2", owner: "o2", desc: ""},
-		{name: "b3", owner: "", desc: "d3"},
-		{name: "b0", owner: "o0", desc: "d0"},
-	    ],
-	};
-	return (
-	    <Branches {...props} />
-	);
+        let props = {
+            descChanged: (repo: string, desc: string) => {
+                return new Promise<void>((s, j) => {
+                    setTimeout(s, 3000);
+                });
+            },
+            ownerChanged: (repo: string, owner: string) => {
+                return new Promise<void>((s, j) => {
+                    setTimeout(s, 3000);
+                });
+            },
+            project: {
+                name: "test/repo",
+                branches: [
+                    { name: "b1", owner: "o1", desc: "d1" },
+                    { name: "b2", owner: "", desc: "d2" },
+                    { name: "b3", owner: "o3", desc: "" },
+                    { name: "b0", owner: "o0", desc: "d0" },
+                ],
+            },
+        };
+        return (
+            <Project {...props} />
+        );
     }
 }
