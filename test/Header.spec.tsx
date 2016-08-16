fbdf498f4d8b77d2f2dcaee789ad4a3952360549
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-16 18:02:39 +0800

    component Header is completed

--- test/Header.spec.tsx
+++ test/Header.spec.tsx
@@ -5,15 +5,41 @@
 
 import * as React from "react";
 import { shallow, ShallowWrapper } from "enzyme";
+import Header, { Props } from "../src/Header";
 
 const expect = chai.expect;
 
+function prop(): Props {
+    return {
+	keywordChanged: sinon.spy(),
+	name: "test",
+	img: "test_image",
+    };
+}
+
 describe("<Header />", () => {
-    it("is div.header");
-    it("has a div.logo");
-    it("shows your company name in div.logo > span");
-    it("has a div.search");
-    it("has a search bar (DebouncedInput) in div.search");
-    it("has a div.user");
-    it("shows an image in div.user");
+
+    // some common tests
+    let wrapper = shallow(<Header {...prop()} />);
+    it("is div.header", () => {
+	expect(wrapper.is("div.header")).to.be.true;
+    });
+    it("has a div.logo", () => {
+	expect(wrapper.find("div.logo")).to.have.length(1);
+    });
+    it("shows your company name in div.logo > span", () => {
+	expect(wrapper.find("div.logo").text()).to.equal("test");
+    });
+    it("has a div.search", () => {
+	expect(wrapper.find("div.search")).to.have.length(1);
+    });
+    it("has a search bar (DebouncedInput) in div.search", () => {
+	expect(wrapper.find("div.search > DebouncedInput")).to.have.length(1);
+    });
+    it("has a div.user", () => {
+	expect(wrapper.find("div.user")).to.have.length(1);
+    });
+    it("shows an image in div.user", () => {
+	expect(wrapper.find("div.user > img").prop("src")).to.equal("test_image");
+    });
 });
