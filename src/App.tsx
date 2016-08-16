84ff1a2f157af22fc89c4510f9051890ea07ff7b
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-16 11:51:36 +0800

    prepare integrated test environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import * as React from "react";
+import Branch from "./Branch";
 
 export default class App extends React.Component<{}, {}> {
     render() {
+	let props = {
+	    descChanged: (desc: string) => {
+		return new Promise<void>((s,j) => {
+		    setTimeout(s, 3000);
+		});
+	    },
+	    ownerChanged: (owner: string) => {
+		return new Promise<void>((s,j) => {
+		    setTimeout(s, 3000);
+		});
+	    },
+	    name: "branch_name",
+	    owner: "test_owner",
+	    desc: "test description",
+	};
 	return (
-	    <h1>Hello, world!</h1>
+	    <Branch {...props} />
 	);
     }
 }
