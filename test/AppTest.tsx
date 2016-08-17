46fea1f72b4edd8eae323b4de1647500d05f4ff6
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-17 15:57:12 +0800

    test if we can update

--- test/AppTest.tsx
+++ test/AppTest.tsx
@@ -9,37 +9,38 @@ import App from "../src/App";
 import { ByMock } from "./FakeAPI";
 
 const expect = chai.expect;
+const defaultChangeset = {
+    modified: [
+	{
+	    name: "Ronmi/react-toy-router",
+	    branches: [
+		{ name: "main", owner: "ronmi", desc: "stable" },
+		{ name: "dev", owner: "ronmi", desc: "develop" },
+	    ],
+	},
+	{
+	    name: "Ronmi/react-promise-visualizer",
+	    branches: [
+		{ name: "main", owner: "ronmi", desc: "stable" },
+		{ name: "dev", owner: "ronmi", desc: "develop" },
+		{ name: "exp", owner: "fraina", desc: "experimental" },
+	    ],
+	},
+	{
+	    name: "Ronmi/some-go-project",
+	    branches: [
+		{ name: "main", owner: "ronmi", desc: "stable" },
+		{ name: "dev", owner: "ronmi", desc: "develop" },
+	    ],
+	},
+    ],
+};
 
 describe("<App />", () => {
     let api = new ByMock();
     
     let wrapper = shallow(<App API={api} />);
-    api.update({
-	modified: [
-	    {
-		name: "Ronmi/react-toy-router",
-		branches: [
-		    { name: "main", owner: "ronmi", desc: "stable" },
-		    { name: "dev", owner: "ronmi", desc: "develop" },
-		],
-	    },
-	    {
-		name: "Ronmi/react-promise-visualizer",
-		branches: [
-		    { name: "main", owner: "ronmi", desc: "stable" },
-		    { name: "dev", owner: "ronmi", desc: "develop" },
-		    { name: "exp", owner: "fraina", desc: "experimental" },
-		],
-	    },
-	    {
-		name: "Ronmi/some-go-project",
-		branches: [
-		    { name: "main", owner: "ronmi", desc: "stable" },
-		    { name: "dev", owner: "ronmi", desc: "develop" },
-		],
-	    },
-	],
-    });
+    api.update(defaultChangeset);
     wrapper.update();
 
     it("is div", () => {
@@ -53,5 +54,41 @@ describe("<App />", () => {
     });
     it("contains correct number of Project elements in div.projects", () => {
         expect(wrapper.find("div.projects > Project")).to.have.length(3);
+    });
+
+    it("merges changeset into it", () => {
+	let wrapper = shallow(<App API={api} />);
+	api.update(defaultChangeset);
+	wrapper.update();
+
+	api.update({
+	    deleted: ["Ronmi/some-go-project"],
+	    modified: [
+		// add another go projects
+		{
+		    name: "Ronmi/another-go-project",
+		    branches: [
+			{ name: "main", owner: "ronmi", desc: "stable" },
+			{ name: "dev", owner: "ronmi", desc: "develop" },
+		    ],
+		},
+		// delete a branch in toy router
+		{
+		    name: "Ronmi/react-toy-router",
+		    branches: [
+			{ name: "main", owner: "ronmi", desc: "stable" },
+		    ],
+		},
+	    ],
+	});
+	wrapper.update();
+
+        expect(wrapper.find("div.projects > Project")).to.have.length(3);
+	expect(wrapper.find("div.projects > Project").findWhere((n) => {
+	    if (!n.is("Project")) return false;
+	    const info = n.prop("project");
+
+	    return info.name === "Ronmi/react-toy-router" && info.branches.length === 1;
+	})).to.have.length(1);
     });
 });
